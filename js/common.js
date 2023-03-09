fa21dbd6e1a967670d65534866ac8a1dcdccafde
Виктория Захарова <zaharova181114@gmail.com>
2023-03-09 15:50:37 +0200

    edit1

--- js/common.js
+++ js/common.js
@@ -1,36 +1,24 @@
-
-// показать карточку товара по клику в зависимости от значениея data-tab
-// $(document).ready(function () {
-// 	$(".js-tab-trigger").click(function () {
-// 		var id = $(this).attr('data-tab'),
-// 			content = $('.js-tab-content[data-tab="' + id + '"]');
-
-// 		$('.js-tab-trigger.active').removeClass('active'); // 1
-// 		$(this).addClass('active'); // 2
-
-// 		$('.js-tab-content.active').removeClass('active'); // 3
-// 		content.addClass('active'); // 4
-// 	});
-// });
-
 $('.main-slider-bg-wrapper').slick({
 	slidesToShow: 1,
-	fade: true,
-	arrows: false
+	// fade: false,
+	// cssEase: 'ease-out',
+	arrows: false,
+	infinite: false,
 });
 
 $('.main-slider').slick({
 	slidesToShow: 1,
+	infinite: false,
 	arrows: false,
-	fade: true,
 	swipe: false,
+	// fade: true,
 	responsive: [
 		{
 			breakpoint: 1200,
 			settings: {
 				slidesToShow: 1,
-				autoplay: true,
-				autoplaySpeed: 2000,
+				// autoplay: true,
+				// autoplaySpeed: 2000,
 				asNavFor: '.main-slider-bg-wrapper'
 			}
 		}
